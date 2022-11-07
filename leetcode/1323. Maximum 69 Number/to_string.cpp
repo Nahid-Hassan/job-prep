021ba3709094521bd5f1a90bb5b1ea49af91ead2
Nahid-Hassan <nahid.cseru@gmail.com>
2022-11-07 13:28:30 +0600

    leetcode - 1323 to_string and stoi

---
+++ leetcode/1323. Maximum 69 Number/to_string.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+class Solution {
+public:
+    int maximum69Number (int num) {
+        string s = to_string(num);
+        
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '6') {
+                s[i] = '9';
+                break;
+            }
+        }
+        num = stoi(s);        
+
+        return num;
+    }
+};

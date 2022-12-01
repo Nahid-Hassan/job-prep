2e2d351c50562f129f20164138067a84999a4ac4
Nahid-Hassan <nahid.cseru@gmail.com>
2022-12-01 18:33:47 +0600

    leetcode - 349 faster solution

---
+++ leetcode/350. Intersection of Two Arrays II/unordered_map.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map <int, int> mp;
+        for (auto num : nums1) mp[num]++;
+        
+        vector<int> sol;
+        for (auto num : nums2) {
+            if (mp[num] > 0) {
+                sol.push_back(num);
+                mp[num] = 0;
+            }
+        }
+        
+        return sol;
+    }
+};

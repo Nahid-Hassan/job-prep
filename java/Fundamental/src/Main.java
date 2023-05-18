22667dc6fd6ad24f4e0511037d7a23f80667d46f
Nahid-Hassan <nahid.cseru@gmail.com>
2023-05-18 10:01:02 +0600

    add company list to apply and java fundamental dir

---
+++ java/Fundamental/src/Main.java
@@ -0,0 +1,15 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        /*
+         Press Alt+Enter with your caret at the highlighted text to see how
+         IntelliJ IDEA suggests fixing it.
+        */
+        System.out.println("Hello and welcome!");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}

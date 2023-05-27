b1822fbf73aeea2f7a4b3ec2e608b8e66c5d28f7
Nahid-Hassan <nahid.cseru@gmail.com>
2023-05-27 18:23:10 +0600

    mysql create database and table and insert values

---
+++ mysql/CreateandInsert.sql
@@ -0,0 +1,17 @@
+-- create database Persons;
+
+-- CREATE TABLE Persons (
+--     PersonID int,
+--     LastName varchar(255),
+--     FirstName varchar(255),
+--     Address varchar(255),
+--     City varchar(255)
+-- ); 
+
+-- insert into Persons
+-- values(1, 'Nahid', 'Hassan', 'Beltia', 'Jamalpur');
+
+-- select * from Persons;
+
+
+
